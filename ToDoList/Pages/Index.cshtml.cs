51c23ecddcf17901d895e7fe7afd18a960403139
aakashsadnani <A00235504@mycambrian.ca>
2021-07-20 19:38:58 -0400

    * Index.cshtml: * _MyPartialFile.cshtml:
    * Index.cshtml.cs: Basic form created with onclick to index page

--- ToDoList/Pages/Index.cshtml.cs
+++ ToDoList/Pages/Index.cshtml.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using ToDoList.Data;
 
 namespace ToDoList.Pages
 {
@@ -20,6 +21,37 @@ namespace ToDoList.Pages
         public void OnGet()
         {
 
+        }
+
+        [FromForm]
+        public Customer Customer { get; set; }
+
+        [FromForm(Name = "loginName")]
+        public string LoginName { get; set; }
+
+        [FromForm(Name = "loginPass")]
+        public string LoginPass { get; set; }
+
+        [FromQuery(Name = "leftNumber")]
+        public int leftNumber { get; set; }
+
+        [FromQuery(Name = "rightNumber")]
+        public int rightNumber { get; set; }
+
+        public int Result { get; set; }
+
+        public bool ResultSet { get; set; }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+           
+
+            return RedirectToPage("./Index");
         }
     }
 }
